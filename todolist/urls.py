2207c92f8a6c7b9e5d2aad24adb7dff30b121da2
fio <fiowind.zfx@gmail.com>
2015-01-21 12:40:29 +0800

    c

---
+++ todolist/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'todolist.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', include('login.urls')),
+    url(r'^todo/', include('todo.urls')),
+    url(r'^$', include('login.urls')),
+)
