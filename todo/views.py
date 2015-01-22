289dd26704e0c04161acfdd4fa26b63f2971db6f
fiowind <fiowind.zfx@gmail.com>
2015-01-22 18:26:24 +0800

    update radio checked bug

--- todo/views.py
+++ todo/views.py
@@ -2,44 +2,48 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.http import Http404
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from todo.models import Todo
- 
- 
+
+
 def TodoView(request):
-    username = request.COOKIES.get('username','')
+    username = request.COOKIES.get('username', '')
     template_name = 'todo/todo.html'
 
     todolist = Todo.objects.filter(flag=1).filter(user=username)
     finishedlist = Todo.objects.filter(flag=0).filter(user=username)
-    template_value = {'todolist': todolist, 'finishedlist': finishedlist,'username':username}
+    template_value = {
+        'todolist': todolist, 'finishedlist': finishedlist, 'username': username}
 
     return render_to_response(template_name, template_value, context_instance=RequestContext(request))
 
+
 def AddView(request):
-    username = request.COOKIES.get('username','')
+    username = request.COOKIES.get('username', '')
     if request.method == "POST":
         thisTodo = request.POST['todo']
         thisPriority = request.POST['priority']
         thisUser = User.objects.get(id=1)
-        todo = Todo(user=username, todo=thisTodo, priority=thisPriority, flag=1)
+        todo = Todo(
+            user=username, todo=thisTodo, priority=thisPriority, flag=1)
         todo.save()
 
         todolist = Todo.objects.filter(flag=1).filter(user=username)
         finishedlist = Todo.objects.filter(flag=0).filter(user=username)
         template_name = 'todo/todo.html'
-        template_value = {'todolist': todolist, 'finishedlist': finishedlist,'username':username}
+        template_value = {
+            'todolist': todolist, 'finishedlist': finishedlist, 'username': username}
         return render_to_response(template_name, template_value, context_instance=RequestContext(request))
-    
+
     else:
         todo = ''
         template_name = 'todo/add.html'
         template_value = {'todo': todo}
         return render_to_response(template_name, template_value, context_instance=RequestContext(request))
-        
+
 
 def DoneView(request, id=''):
-    username = request.COOKIES.get('username','')
+    username = request.COOKIES.get('username', '')
     todo = Todo.objects.get(id=id)
     if todo.flag == 1:
         todo.flag = 0
@@ -49,12 +53,13 @@ def DoneView(request, id=''):
     todolist = Todo.objects.filter(flag=1).filter(user=username)
     finishedlist = Todo.objects.filter(flag=0).filter(user=username)
     template_name = "todo/todo.html"
-    template_value = {'todolist': todolist, 'finishedlist': finishedlist,'username':username}
+    template_value = {
+        'todolist': todolist, 'finishedlist': finishedlist, 'username': username}
     return render_to_response(template_name, template_value, context_instance=RequestContext(request))
 
 
 def UndoView(request, id=''):
-    username = request.COOKIES.get('username','')
+    username = request.COOKIES.get('username', '')
     todo = Todo.objects.get(id=id)
     if todo.flag == 0:
         todo.flag = 1
@@ -64,11 +69,13 @@ def UndoView(request, id=''):
     todolist = Todo.objects.filter(flag=1).filter(user=username)
     finishedlist = Todo.objects.filter(flag=0).filter(user=username)
     template_name = "todo/todo.html"
-    template_value = {'todolist': todolist, 'finishedlist': finishedlist,'username':username}
+    template_value = {
+        'todolist': todolist, 'finishedlist': finishedlist, 'username': username}
     return render_to_response(template_name, template_value, context_instance=RequestContext(request))
 
+
 def DeleteView(request, id=''):
-    username = request.COOKIES.get('username','')
+    username = request.COOKIES.get('username', '')
     try:
         todo = Todo.objects.get(id=id)
     except Exception:
@@ -80,11 +87,12 @@ def DeleteView(request, id=''):
 
     todolist = Todo.objects.filter(flag=1).filter(user=username)
     template_name = "todo/todo.html"
-    template_value = {'todolist': todolist,'username':username}
+    template_value = {'todolist': todolist, 'username': username}
     return render_to_response(template_name, template_value, context_instance=RequestContext(request))
 
+
 def EditView(request, id=''):
-    username = request.COOKIES.get('username','')
+    username = request.COOKIES.get('username', '')
     try:
         todo = Todo.objects.get(id=id)
     except Exception:
@@ -98,12 +106,20 @@ def EditView(request, id=''):
         todolist = Todo.objects.filter(flag=1).filter(user=username)
         finishedlist = Todo.objects.filter(flag=0).filter(user=username)
         template_name = 'todo/todo.html'
-        template_value = {'todolist': todolist, 'finishedlist': finishedlist,'username':username}
-        return render_to_response(template_name, template_value, context_instance=RequestContext(request))
-    
-    else:
-        template_name = 'todo/edit.html'
-        template_value = {'todo': todo}
+        template_value = {
+            'todolist': todolist, 'finishedlist': finishedlist, 'username': username}
         return render_to_response(template_name, template_value, context_instance=RequestContext(request))
 
- 
+    # else:
+    #     template_name = 'todo/edit.html'
+    #     template_value = {'todo': todo}
+    #     return render_to_response(template_name, template_value, context_instance=RequestContext(request))
+    else:
+        try:
+            todo = Todo.objects.get(id=id)
+            template_name = 'todo/edit.html'
+            template_value = {'todo': todo}
+        except Exception:
+            raise Http404
+        return render_to_response(template_name, template_value,
+                                  context_instance=RequestContext(request))
